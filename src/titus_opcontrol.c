9eefdae69ab6ffaa80e0c0363f20d4118bd7b5ba
Elijah-Barker <itguy4techclass@gmail.com>
2016-01-28 20:23:46 -0500

    Op Control working perfectly (first try!)

--- src/titus_opcontrol.c
+++ src/titus_opcontrol.c
@@ -21,36 +21,58 @@ void controlmotors(int lb, int rb,int lf,int rf){
 	}
 }
 
-void titus_controldrive(int t, int s, int f) {
-	double straif=f;
-	double turn=t;
-	double forward=f;
+void titus_controldrive(int t, int f, int s) {
+	int strafe=s;
+	int turn=t;
+	int forward=f;
 	int lf,lb,rf,rb;
+
+	int forward_lf = 1  * forward;
+	int forward_lb = 1  * forward;
+	int forward_rf = -1 * forward;
+	int forward_rb = -1 * forward;
+
+	int sideways_lf = 1  * strafe;
+	int sideways_lb = -1 * strafe;
+	int sideways_rf = 1  * strafe;
+	int sideways_rb = -1 * strafe;
+
+
+	/*
 	float pi = 3.14;
 
 	rf=lb=straif*cos(-pi/4)-forward*sin(-pi/4);
 	lb=-lb;
 	rb=lf=straif*sin(-pi/4)+forward*cos(-pi/4);
 	lf=-lf;
-
-/*	rf=lb=straif;
+	rf=lb=straif;
 	lb=-lb;
 	rb=lf=forward;
-	lf=-lf;*/
-	rb+=turn;
-	rf-=turn;
-	lf-=turn;
+	lf=-lf;
+	//*/
+
+
+	lf = forward_lf + sideways_lf;
+	lb = forward_lb + sideways_lb;
+	rf = forward_rf + sideways_rf;
+	rb = forward_rb + sideways_rb;
+
+	//*/
+	lf+=turn;
 	lb+=turn;
+	rf+=turn;
+	rb+=turn;
 
 	controlmotors(lb, rb, lf, rf); 
 }
 
 void driveoperation() {
 	int joyforward = (abs(joystickGetAnalog(1,JOY_FORWARD_T)) > JOY_DEAD_T) ? joystickGetAnalog(1,JOY_FORWARD_T) : 0;
+	int joyforward2 = (abs(joystickGetAnalog(1,JOY_FORWARD2_T)) > JOY_DEAD_T) ? joystickGetAnalog(1,JOY_FORWARD2_T) : 0;
 	int joyturn = (abs((joystickGetAnalog(1,JOY_TURN_T))) > JOY_DEAD_T) ? joystickGetAnalog(1,JOY_TURN_T) : 0;
 	int joystrafing = (abs(joystickGetAnalog(1,JOY_STRAFING)) > JOY_DEAD_T) ? joystickGetAnalog(1,JOY_STRAFING) : 0;
 
-	titus_controldrive(joyturn,joyforward,joystrafing);
+	titus_controldrive(joyturn,joyforward+joyforward2,joystrafing);
 }
 
 void operatorControl() {
